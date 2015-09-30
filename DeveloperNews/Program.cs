a92d8f4865e3a074c1b7da475710166db2c43425
xland <412588801@qq.com>
2015-09-30 08:48:01 +0800

    增加了oschina和36kr的采集程序

--- DeveloperNews/Program.cs
+++ DeveloperNews/Program.cs
@@ -20,6 +20,12 @@ namespace DeveloperNews
         {
             dbFactory = new OrmLiteConnectionFactory(ConfigurationManager.AppSettings["dbConnStr"], MySqlDialect.Provider);
 
+            var Tkr36 = new Thread(new ThreadStart(kr36));
+            Tkr36.Start();
+
+            var Toschina = new Thread(new ThreadStart(oschina));
+            Toschina.Start();
+
             var Tinfoq = new Thread(new ThreadStart(infoq));
             Tinfoq.Start();
 
@@ -42,6 +48,96 @@ namespace DeveloperNews
                 return true;
             }
             return false;
+        }
+
+        static void kr36()
+        {
+            CQ doc;
+            try
+            {
+                var client = new RestClient("http://36kr.com/");
+                var resq = new RestRequest(Method.GET);
+                var resp = client.Execute(resq);
+                doc = resp.Content;
+            }
+            catch (Exception ex)
+            {
+                Thread.Sleep(GetWaitTime());
+                kr36();
+                return;
+            }
+            var arr = doc["article .desc"].ToList();
+            var dataList = new List<allen_news>();
+            var db = dbFactory.Open();
+            foreach (var item in arr)
+            {
+                CQ target = item.InnerHTML;
+                var data = new allen_news();
+                data.news_title = target[".title"].Text().Trim();
+                if (checkTitle(data.news_title))
+                {
+                    continue;
+                }
+                data.news_summary = target[".brief"].Text().Trim();
+                data.author = target[".author a"].Text().Trim();
+                data.add_time = DateTime.Now;
+                data.from_site_flag = 3;
+                data.news_url = "http://36kr.com" + target[".title"].Attr("href");
+                dataList.Insert(0, data);
+            }
+            if (dataList.Count > 0)
+            {
+                db.InsertAll<allen_news>(dataList);
+            }
+            db.Dispose();
+            Console.WriteLine("增加了{0}条文章3", dataList.Count);
+            Thread.Sleep(GetWaitTime());
+            kr36();
+        }
+
+        static void oschina()
+        {
+            CQ doc;
+            try
+            {
+                var client = new RestClient("http://www.oschina.net/news/list");
+                var resq = new RestRequest(Method.GET);
+                var resp = client.Execute(resq);
+                doc = resp.Content;
+            }
+            catch (Exception ex)
+            {
+                Thread.Sleep(GetWaitTime());
+                oschina();
+                return;
+            }
+            var arr = doc["#RecentNewsList .List li"].ToList();
+            var dataList = new List<allen_news>();
+            var db = dbFactory.Open();
+            foreach (var item in arr)
+            {
+                CQ target = item.InnerHTML;
+                var data = new allen_news();
+                data.news_title = target["h2 a"].Text().Trim();
+                if (checkTitle(data.news_title))
+                {
+                    continue;
+                }
+                data.news_summary = target[".detail"].Text().Trim();
+                data.author = target[".date a"].Text().Trim();
+                data.add_time = DateTime.Now;
+                data.from_site_flag = 2;
+                data.news_url = "http://www.oschina.net" + target["h2 a"].Attr("href");
+                dataList.Insert(0, data);
+            }
+            if (dataList.Count > 0)
+            {
+                db.InsertAll<allen_news>(dataList);
+            }
+            db.Dispose();
+            Console.WriteLine("增加了{0}条文章2", dataList.Count);
+            Thread.Sleep(GetWaitTime());
+            oschina();
         }
         static void infoq()
         {
@@ -69,7 +165,7 @@ namespace DeveloperNews
                 data.news_title = target["h2 a"].Text().Trim();
                 if (checkTitle(data.news_title))
                 {
-                    break;
+                    continue;
                 }
                 data.news_summary = target["p"].Text().Trim();
                 data.author = target[".author a"].Text().Trim();
@@ -114,7 +210,7 @@ namespace DeveloperNews
                 data.news_title = strArr[0];
                 if (checkTitle(data.news_title))
                 {
-                    break;
+                    continue;
                 }
                 data.news_summary = strArr[1].Trim();
                 data.author = strArr[2].Split("发布于".ToCharArray(),StringSplitOptions.RemoveEmptyEntries)[0];
